92c51883464085e9cf1cb1d42167e55fb58f935d
mohanak <mohanak@mkcl.org>
2021-05-15 19:40:33 +0530

    Added Login Functionality

--- Server/controller/controller.go
+++ Server/controller/controller.go
@@ -6,11 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Users = make(map[string]string) //temp db
+
 func SignUp(c *gin.Context){
 	user := model.User{}
 	if err := c.ShouldBindJSON(&user);err!=nil{
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 	}
+	Users[user.Email]=user.Password
 	c.JSON(http.StatusOK,gin.H{"status":"Successfully signed up"})
+}
+
+func Login(c *gin.Context){
+	userLogin := model.UserLogin{}
+	if err := c.ShouldBindJSON(&userLogin);err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+	}
+
+	if userLogin.Password != Users[userLogin.Email]{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized credentials"})
+			return
+	}
+
+	c.JSON(http.StatusOK,gin.H{"status":"Successfully Logged in"})
 }
