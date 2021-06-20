dc8b506c2ec9a3aa1d1a95ac92721394c82978f7
mohanak <mohanak@mkcl.org>
2021-06-20 18:51:48 +0530

    main file

--- Client/oes/src/main.js
+++ Client/oes/src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import VueCookies from "vue-cookies";
+// Link to css
+// import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/scss/bootstrap-custom.scss";
+import "../node_modules/vue-multiselect/dist/vue-multiselect.min.css";
+import "./assets/plugins/materialdesignicons/css/materialdesignicons.min.css";
+// import "swiper/dist/css/swiper.css";
+import "./assets/scss/template.scss";
+Vue.config.productionTip = false;
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+Vue.use(VueAwesomeSwiper);
+Vue.use(VueCookies);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
