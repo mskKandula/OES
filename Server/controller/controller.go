73d4baaf451cc60a69feae7367f7fbad51af358d
mohanak <mohanak@mkcl.org>
2021-06-28 10:02:43 +0530

    Student Model & Controller

--- Server/controller/controller.go
+++ Server/controller/controller.go
@@ -1,57 +1,58 @@
 package controller
 
 import (
-	"fmt"
-	"net/http"
-	"bytes"
 	"bufio"
-	"strings"
+	"bytes"
+	"fmt"
 	"io"
-	"github.com/mskKandula/model"
-	"github.com/mskKandula/middleware"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/mskKandula/middleware"
+	"github.com/mskKandula/model"
 )
 
 var Users = make(map[string]string) //temp db
 
 var fileTextLines []string
 
-func SignUp(c *gin.Context){
+func SignUp(c *gin.Context) {
 	user := model.User{}
-	if err := c.ShouldBindJSON(&user);err!=nil{
+	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+		return
 	}
-	Users[user.Email]=user.Password
-	c.JSON(http.StatusOK,gin.H{"status":"Successfully signed up"})
+	Users[user.Email] = user.Password
+	c.JSON(http.StatusOK, gin.H{"status": "Successfully signed up"})
 }
 
-func Login(c *gin.Context){
+func Login(c *gin.Context) {
 	userLogin := model.UserLogin{}
-	if err := c.ShouldBindJSON(&userLogin);err!=nil{
+	if err := c.ShouldBindJSON(&userLogin); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+		return
 	}
 
-	if userLogin.Password != Users[userLogin.Email]{
+	if userLogin.Password != Users[userLogin.Email] {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-			return
+		return
 	}
 
-	token,time,err := middleware.Auth(userLogin)
+	token, time, err := middleware.Auth(userLogin)
 
-	if err!=nil{
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+		return
 	}
 
-	c.JSON(http.StatusOK,gin.H{"token":token,"expirationTime":time})
+	c.JSON(http.StatusOK, gin.H{"token": token, "expirationTime": time})
 }
 
-func Questionhandle(c *gin.Context){
+func Questionhandle(c *gin.Context) {
 
-	file,handler, err := c.Request.FormFile("myFile")
-	
+	file, handler, err := c.Request.FormFile("myFile")
+
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
 		fmt.Println(err)
@@ -69,22 +70,27 @@ func Questionhandle(c *gin.Context){
 	}
 
 	buf := bytes.NewBuffer(nil)
-    if _,err = io.Copy(buf, file); err != nil {
-    fmt.Println(err)
-	return
-}
+	if _, err = io.Copy(buf, file); err != nil {
+		fmt.Println(err)
+		return
+	}
 	fileScanner := bufio.NewScanner(buf)
 
 	fileScanner.Split(bufio.ScanLines)
- 
+
 	for fileScanner.Scan() {
 		fileTextLines = append(fileTextLines, fileScanner.Text())
 	}
 
-	c.JSON(http.StatusOK,gin.H{"Questions":fileTextLines})
+	c.JSON(http.StatusOK, gin.H{"Questions": fileTextLines})
 
 }
 
-func GetQuestions(w http.ResponseWriter,r *http.Request){
-	c.JSON(http.StatusOK,gin.H{"Questions":fileTextLines})
+func GetQuestions(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Questions": fileTextLines})
+}
+
+func GetStudents(c *gin.Context) {
+	var students []model.Student
+	c.JSON(http.StatusOK, gin.H{"students": students})
 }
