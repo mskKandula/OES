e2b941ec9f95a92c46a24a05b5e6e09700b040df
mohanak <mohanak@mkcl.org>
2021-08-26 18:59:51 +0530

    Speech to text & Virtual Keyboard in online exam.

---
+++ Client/oes/src/views/Exam.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="main-content">
+    <div id="examId">
+      <h1>Online Exam</h1>
+      <hr />
+      <button class="btn btn-primary" v-on:click="key = !key">
+        Virtual keyboard</button
+      >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button class="btn btn-primary" @click="startTxtToSpeech">Speak</button>
+      <!-- <div v-for="(word, index) in transcription_" :key="index">
+        {{ word }}
+        
+      </div> -->
+
+      <div v-for="(question, index) in users" :key="index">
+        <div v-show="index === questionIndex">
+          <h4>{{ questionIndex + 1 }}:{{ question }}</h4>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            :value="input"
+            @input="onInputChange"
+          ></textarea>
+
+          <br />
+
+          <button
+            class="btn btn-theme btn-rounded mx-auto"
+            v-if="questionIndex > 0"
+            v-on:click="prev"
+          >
+            prev
+          </button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button
+            class="btn btn-theme btn-rounded mx-auto"
+            v-if="questionIndex < users.length - 1"
+            v-on:click="next"
+          >
+            next
+          </button>
+          <button
+            class="btn btn-theme btn-rounded mx-auto"
+            v-if="index == users.length - 1"
+            @click="submitAnswer"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+    <SimpleKeyboard
+      v-if="this.key"
+      @onChange="onChange"
+      @onKeyPress="onKeyPress"
+      :input="input"
+    />
+  </div>
+</template>
+
+<script>
+// import usersData from "./users.json";
+import SimpleKeyboard from "./SimpleKeyboard";
+import "./App.css";
+import axios from "axios";
+export default {
+  components: {
+    SimpleKeyboard,
+  },
+  data() {
+    return {
+      runtimeTranscription_: "",
+      transcription_: [],
+      lang_: "en_US",
+      input: "",
+      key: false,
+      users: [],
+      questionIndex: 0,
+      answer: [],
+      test: "",
+      current: "",
+      res: "",
+    };
+  },
+  methods: {
+    next: function () {
+      this.questionIndex++;
+      if (this.input.length > 0) {
+        this.answer.push(this.input);
+        this.input = "";
+      } else {
+        this.input = "";
+        this.answer.push(this.input);
+      }
+    },
+    // Go to previous question
+    prev: function () {
+      this.questionIndex--;
+    },
+    submitAnswer() {
+      let self = this;
+      self.answer.push(self.input);
+      self.res = this.$go.countser(JSON.stringify({ ans: self.answer }));
+      if (self.res) {
+        self.$router.push({ name: "WordCounter", params: { count: self.res } });
+      }
+    },
+    onChange(input) {
+      this.input = input;
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    },
+    startTxtToSpeech() {
+      // initialisation of voicereco
+      window.SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new window.SpeechRecognition();
+      recognition.lang = this.lang_;
+      recognition.interimResults = true;
+      // event current voice reco word
+      recognition.addEventListener("result", (event) => {
+        //  console.log("event",event)
+        this.current = event.results[0][0].transcript;
+        // console.log("this.current",this.current)
+        // const text = Array.from(event.results)
+        //   .map(result => result[0])
+        //   .map(result => result.transcript)
+        //   .join("");
+        // this.runtimeTranscription_ = text;
+        // console.log("this.runtimeTranscription_",this.runtimeTranscription_)
+        // console.log("text",text)
+      });
+      // end of transcription
+      recognition.addEventListener("end", () => {
+        // this.transcription_.push(this.runtimeTranscription_);
+        // console.log("this.transcription_",this.transcription_)
+        this.input += this.current;
+        this.runtimeTranscription_ = "";
+        recognition.stop();
+      });
+      recognition.start();
+    },
+    startSpeechToTxt() {
+      // start speech to txt
+      var utterance = new SpeechSynthesisUtterance("Message Envoyé");
+      window.speechSynthesis.speak(utterance);
+    },
+    getQues() {
+      let self = this;
+      axios
+        .get("/getQuestions")
+        .then(function (res) {
+          if (res.data) {
+            self.users = res.data.files;
+          }
+        })
+        .catch(function () {
+          console.log("FAILURE!!");
+        });
+    },
+  },
+  mounted() {
+    this.getQues();
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Client/oes/src/views/SimpleKeyboard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="keyboardClass"></div>
+</template>
+
+<script>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+import layout from "simple-keyboard-layouts/build/layouts/english";
+
+export default {
+  name: "SimpleKeyboard",
+  props: {
+    keyboardClass: {
+      default: "simple-keyboard",
+      type: String
+    },
+    input: {
+      type: String
+    }
+  },
+  data: () => ({
+    keyboard: null
+  }),
+  mounted() {
+    this.keyboard = new Keyboard(this.keyboardClass, {
+      onChange: this.onChange,
+      onKeyPress: this.onKeyPress,
+      ...layout
+    });
+  },
+  methods: {
+    onChange(input) {
+      this.$emit("onChange", input);
+    },
+    onKeyPress(button) {
+      this.$emit("onKeyPress", button);
+
+      /**
+       * If you want to handle the shift and caps lock buttons
+       */
+      if (button === "{shift}" || button === "{lock}") this.handleShift();
+    },
+    handleShift() {
+      let currentLayout = this.keyboard.options.layoutName;
+      let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+      this.keyboard.setOptions({
+        layoutName: shiftToggle
+      });
+    }
+  },
+  watch: {
+    input(input) {
+      this.keyboard.setInput(input);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
