10dd0ea6b12386030c01663d1f6e465c88eece1c
mohanak <mohanak@mkcl.org>
2022-05-28 17:27:00 +0530

    HLS Player

--- Client/oes/src/views/VideoContent.vue
+++ Client/oes/src/views/VideoContent.vue
@@ -3,24 +3,28 @@
 
 <h2>Videos List</h2>
 
-  <div v-for="(video,index) in this.videosList"
-  :key="index">{{video}}
-  <video id="video" width="320" height="240" controls @click="playVideo(video.VideoUrl)">{{video.VideoUrl}}
+  <!-- <div v-for="(vid,index) in this.videosList"
+  :key="index"> -->
+  <video width="320" height="240" ref="videoRef" controls>
+    <!-- <source :src="vid.VideoUrl" type="application/x-mpegURL"> -->
 </video>
-  </div>
+  <!-- </div> -->
   </div>
 </template>
 <script>
 import axios from "axios";
+import Hls from 'hls.js';
+
 export default {
   data() {
     return {
       videosList: [{
           Name : "docker",
-          VideoUrl:"/assets/Videos/docker/index.m3u8",
+          VideoUrl:"https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
           ThumbnailPath:"",
           Description:"Docker"
-      }]
+      }],
+      hls: null
     };
   },
   methods:{
@@ -30,7 +34,6 @@ export default {
         .get("/getVideos")
         .then(function(res) {
           if (res.data) {
-            console.log("26",res)
             self.videosList = res.data.videos;
           }
         })
@@ -38,26 +41,42 @@ export default {
           console.log("FAILURE!!");
         });
      },
-     playVideo(url) {
-          var video = document.getElementById('video');
-  if(Hls.isSupported()) {
-      console.log("44",url)
-    var hls = new Hls();
-    hls.loadSource(url);
-    hls.attachMedia(video);
-  //   hls.on(Hls.Events.MANIFEST_PARSED,function() {
+     playVideo(ref) {
+  //      console.log(ref)
+  //  let video = this.$refs[ref];
+  //   this.hls.loadSource("http://127.0.0.1:8887/index.m3u8");
+  //     console.log("37-----------",this.hls)
+  //   this.hls.attachMedia(video);
+  //   this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
+  //     console.log("41-----------")
   //     video.play();
-  // });
- } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = url;
-    // video.addEventListener('loadedmetadata',function() {
-    //   video.play();
-    // });
-  }
+  //   });
+//           var video = document.getElementById('video');
+//   if(Hls.isSupported()) {
+//       console.log("44",url)
+//     var hls = new Hls();
+//     hls.loadSource(url);
+//     hls.attachMedia(video);
+//     hls.on(Hls.Events.MANIFEST_PARSED,function() {
+//       video.play();
+//   });
+//  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+//     video.src = url;
+//     video.addEventListener('loadedmetadata',function() {
+//       video.play();
+//     });
+//   }
      }
      },
  mounted() {
     //  this.getVideos();
+    this.hls = new Hls();
+    let video = this.$refs["videoRef"];
+    this.hls.loadSource("http://127.0.0.1:8887/docker/index.m3u8");
+    this.hls.attachMedia(video);
+    this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play();
+    });
 }
 }
 </script>
