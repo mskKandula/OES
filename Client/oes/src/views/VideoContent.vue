df8cec2ba21f35a27af192c53654d8c9b7d397b2
mohanak <mohanak@mkcl.org>
2022-06-01 17:52:27 +0530

    Separated out hls functionality

--- Client/oes/src/views/VideoContent.vue
+++ Client/oes/src/views/VideoContent.vue
@@ -3,28 +3,22 @@
 
 <h2>Videos List</h2>
 
-  <!-- <div v-for="(vid,index) in this.videosList"
-  :key="index"> -->
-  <video width="320" height="240" ref="videoRef" controls>
-    <!-- <source :src="vid.VideoUrl" type="application/x-mpegURL"> -->
-</video>
-  <!-- </div> -->
+<div class="row" v-for="(vid,index) in this.videosList"
+  :key="index"> 
+  <div class="col-4">
+  <img :src="'../../../../'+ vid.thumbnailPath" :alt="vid.description" width="300" height="300" @click="playVideo(vid.videoUrl)">
+    </div> 
+  </div>
   </div>
 </template>
 <script>
 import axios from "axios";
-import Hls from 'hls.js';
 
 export default {
   data() {
     return {
-      videosList: [{
-          Name : "docker",
-          VideoUrl:"https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-          ThumbnailPath:"",
-          Description:"Docker"
-      }],
-      hls: null
+      videosList: [],
+  
     };
   },
   methods:{
@@ -41,42 +35,12 @@ export default {
           console.log("FAILURE!!");
         });
      },
-     playVideo(ref) {
-  //      console.log(ref)
-  //  let video = this.$refs[ref];
-  //   this.hls.loadSource("http://127.0.0.1:8887/index.m3u8");
-  //     console.log("37-----------",this.hls)
-  //   this.hls.attachMedia(video);
-  //   this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
-  //     console.log("41-----------")
-  //     video.play();
-  //   });
-//           var video = document.getElementById('video');
-//   if(Hls.isSupported()) {
-//       console.log("44",url)
-//     var hls = new Hls();
-//     hls.loadSource(url);
-//     hls.attachMedia(video);
-//     hls.on(Hls.Events.MANIFEST_PARSED,function() {
-//       video.play();
-//   });
-//  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-//     video.src = url;
-//     video.addEventListener('loadedmetadata',function() {
-//       video.play();
-//     });
-//   }
+     playVideo(url) {
+      this.$router.push({  name: 'VideoPlay', params: { videoUrl : url } })
      }
      },
  mounted() {
-    //  this.getVideos();
-    this.hls = new Hls();
-    let video = this.$refs["videoRef"];
-    this.hls.loadSource("http://127.0.0.1:8887/docker/index.m3u8");
-    this.hls.attachMedia(video);
-    this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      video.play();
-    });
+     this.getVideos();
 }
 }
 </script>
