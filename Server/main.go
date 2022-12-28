486a4858a1be6b74816d6f6ca52435cd9346eafd
mskKandula <mskKandula@gmail.com>
2022-12-28 15:17:05 +0530

    Increase Resource Limitations

--- Server/main.go
+++ Server/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -37,6 +38,16 @@ var (
 
 func main() {
 	fmt.Println("Lets start OES")
+
+	// Increase resources limitations
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
 
 	if err = cnf.Setup("config.json"); err != nil {
 		log.Fatalf("Setup Failed:%v", err.Error())
