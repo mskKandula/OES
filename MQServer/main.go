01386adb49d3f96e0ae418b63c940d958329b0d1
mskKandula <mskKandula@gmail.com>
2022-12-29 18:27:26 +0530

    Docker - Creation & Configuration

--- MQServer/main.go
+++ MQServer/main.go
@@ -18,7 +18,7 @@ func failOnError(err error, msg string) {
 
 func main() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@messageq/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
