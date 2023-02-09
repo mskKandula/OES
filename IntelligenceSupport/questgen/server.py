5e5921a12f0bd81ff34e5fc7ee7dce1cdab072a8
mskKandula <mskKandula@gmail.com>
2023-02-09 12:36:15 +0530

    Requirements & Folder structure changes

--- IntelligenceSupport/questgen/server.py
+++ IntelligenceSupport/questgen/server.py
@@ -1,12 +1,17 @@
+import sys
+
+# setting path
+sys.path.append('./pb')
+
 import grpc
 from concurrent import futures
 import time
-import questgen_pb2 as pb2
-import questgen_pb2_grpc as pb2_grpc
+from pb.questgen_pb2 import QuestGenResponse
+from pb.questgen_pb2_grpc import QuestGenServiceServicer, add_QuestGenServiceServicer_to_server
 
 
 
-class QuestGenerator(pb2_grpc.QuestGenServiceServicer):
+class QuestGenerator(QuestGenServiceServicer):
 
     def __init__(self, *args, **kwargs):
         pass
@@ -18,12 +23,12 @@ class QuestGenerator(pb2_grpc.QuestGenServiceServicer):
         result = f'Hello I am up and running received "{requestData}" message from you'
         result = {'response': result}
 
-        return pb2.QuestGenResponse(**result)
+        return QuestGenResponse(**result)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_QuestGenServiceServicer_to_server(QuestGenerator(), server)
+    add_QuestGenServiceServicer_to_server(QuestGenerator(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
