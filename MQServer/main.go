055928db6ad9643dd26dbe55d2722ad10c8e2954
mskKandula <mskKandula@gmail.com>
2023-04-03 11:54:00 +0530

    For single res

--- MQServer/main.go
+++ MQServer/main.go
@@ -65,18 +65,18 @@ func HlsVideoConversion(result string) {
 	dir, file := filepath.Split(result)
 
 	// For single(original resolution)
-	// cmd := exec.Command("ffmpeg", "-i", paths[4], "-codec:", "copy", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", "index.m3u8")
+	cmd := exec.Command("ffmpeg", "-i", result, "-codec:", "copy", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", dir+"index.m3u8")
 
 	// For Multiple(360p,480p & 720p resolutions)
-	cmd := exec.Command("ffmpeg", "-i", result, "-map", "0:v:0", "-map", "0:a:0", "-map",
-		"0:v:0", "-map", "0:a:0", "-map", "0:v:0", "-map", "0:a:0", "-c:v", "libx264", "-crf",
-		"22", "-c:a", "aac", "-ar", "48000", "-filter:v:0", "scale=w=480:h=360", "-maxrate:v:0",
-		"600k", "-b:a:0", "64k", "-filter:v:1", "scale=w=640:h=480", "-maxrate:v:1", "900k",
-		"-b:a:1", "128k", "-filter:v:2", "scale=w=1280:h=720", "-maxrate:v:2", "1500k", "-b:a:2",
-		"128k", "-var_stream_map", "v:0,a:0,name:360p v:1,a:1,name:480p v:2,a:2,name:720p",
-		"-preset", "slow", "-hls_list_size", "0", "-threads", "0", "-f", "hls", "-hls_playlist_type",
-		"event", "-hls_time", "10", "-hls_flags", "independent_segments", "-master_pl_name",
-		"index.m3u8", "-y", dir+"%v/index.m3u8")
+	// cmd := exec.Command("ffmpeg", "-i", result, "-map", "0:v:0", "-map", "0:a:0", "-map",
+	// 	"0:v:0", "-map", "0:a:0", "-map", "0:v:0", "-map", "0:a:0", "-c:v", "libx264", "-crf",
+	// 	"22", "-c:a", "aac", "-ar", "48000", "-filter:v:0", "scale=w=480:h=360", "-maxrate:v:0",
+	// 	"600k", "-b:a:0", "64k", "-filter:v:1", "scale=w=640:h=480", "-maxrate:v:1", "900k",
+	// 	"-b:a:1", "128k", "-filter:v:2", "scale=w=1280:h=720", "-maxrate:v:2", "1500k", "-b:a:2",
+	// 	"128k", "-var_stream_map", "v:0,a:0,name:360p v:1,a:1,name:480p v:2,a:2,name:720p",
+	// 	"-preset", "slow", "-hls_list_size", "0", "-threads", "0", "-f", "hls", "-hls_playlist_type",
+	// 	"event", "-hls_time", "10", "-hls_flags", "independent_segments", "-master_pl_name",
+	// 	"index.m3u8", "-y", dir+"%v/index.m3u8")
 
 	err := cmd.Run()
 
