7649856fe76995241a9ed4094e3673f4d166697d
mskKandula <mskKandula@gmail.com>
2023-09-18 09:49:00 +0530

    HTTPS To Support navigator.mediaDevices

--- Client/oes/vue.config.js
+++ Client/oes/vue.config.js
@@ -1,24 +1,25 @@
 
 module.exports = {
- devServer: {
-    port: 8085, // CHANGE YOUR PORT HERE!
-    proxy: {
-        '/api': {
+    devServer: {
+        port: 8085, // CHANGE YOUR PORT HERE!
+        https: true,
+        proxy: {
+            '/api': {
                 target: 'http://localhost:9000', // provide proxy  for your project
                 ws: true,
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
+                }
+            },
+            '/cdn': {
+                target: 'http://localhost:8887/', // provide proxy  for your project
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/cdn': ''
+                }
             }
-        },
-        '/cdn': {
-            target: 'http://localhost:8887/', // provide proxy  for your project
-            ws: true,
-            changeOrigin: true,
-            pathRewrite: {
-                '^/cdn': ''
         }
     }
-    }
-}
 }
