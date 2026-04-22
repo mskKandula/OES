a473a6b6b3ed69b44c1bfef282fcb6d0e1dfca64
Mohana sai krishna Kandula <73701479+mskKandula@users.noreply.github.com>
2026-04-22 14:40:24 +0530

    Change Clients map to nested structure with mutex
    Refactor Client management to use a nested map structure for better organization and access.

--- Server/util/websock/pool.go
+++ Server/util/websock/pool.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
+	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 	"github.com/mailru/easygo/netpoll"
 	ds "github.com/mskKandula/oes/dataSources"
@@ -28,16 +30,17 @@ var (
 type Pool struct {
 	Register   chan *Client
 	Unregister chan *Client
-	Clients    map[string][]*Client
+	Clients    map[string]map[int]*Client
 	Broadcast  chan []byte
+	mu         sync.RWMutex // Protects Clients map
 }
 
 func init() {
 	poolInit = &Pool{
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
-		Clients:    make(map[string][]*Client),
-		Broadcast:  make(chan []byte),
+		Clients:    make(map[string]map[int]*Client),
+		Broadcast:  make(chan []byte, 256),
 	}
 	ClientConnChan = make(chan *Client, 200)
 }
@@ -65,9 +68,18 @@ func (pool *Pool) Start(ds *ds.DataSources) {
 
 		case newClient := <-pool.Register:
 
-			pool.Clients[newClient.Id] = append(pool.Clients[newClient.Id], newClient)
+			// Initialize nested map if it doesn't exist
+			pool.mu.Lock()
+			if pool.Clients[newClient.Id] == nil {
+				pool.Clients[newClient.Id] = make(map[int]*Client)
+			}
 
-			fmt.Println("Size of Connection Pool: ", len(pool.Clients[newClient.Id]))
+			// Add client using UserId as key in the nested map
+			pool.Clients[newClient.Id][newClient.UserId] = newClient
+			poolSize := len(pool.Clients[newClient.Id])
+			pool.mu.Unlock()
+
+			fmt.Println("Size of Connection Pool: ", poolSize)
 
 			// Get netpoll descriptor with EventRead|EventEdgeTriggered.
 			desc := netpoll.Must(netpoll.HandleRead(newClient.Conn))
@@ -82,91 +94,81 @@ func (pool *Pool) Start(ds *ds.DataSources) {
 				ClientConnChan <- newClient
 			})
 
-			// for _, client := range pool.Clients[client.Id] {
-
-			byteData, err := json.Marshal(Message{Type: 5, Body: map[string]interface{}{"user": newClient.UserId, "add": true}, Id: newClient.Id})
+			rawBody, _ := json.Marshal(JoinLeaveBody{User: newClient.UserId, Add: true})
+			byteData, err := json.Marshal(Message{Type: 5, Body: json.RawMessage(rawBody), Id: newClient.Id})
 			if err != nil {
 				log.Println(err)
 				return
 			}
 
-			for _, client := range pool.Clients[newClient.Id] {
-
-				if client.Role != newClient.Role || client.UserId != newClient.UserId {
-
-					if err := wsutil.WriteServerText(client.Conn, byteData); err != nil {
-						log.Println(err)
-						return
-					}
-
-					// client.Conn.WriteJSON(Message{Type: 5, Body: map[string]interface{}{"user": newClient.UserId, "add": true}, Id: newClient.Id})
-				}
-
-			}
+			pool.Broadcast <- byteData
 
 		case newClient := <-pool.Unregister:
 
-			var toDelete int
-			byteData, err := json.Marshal(Message{Type: 5, Body: map[string]interface{}{"user": newClient.UserId, "add": false}, Id: newClient.Id})
+			rawBody, _ := json.Marshal(JoinLeaveBody{User: newClient.UserId, Add: false})
+			byteData, err := json.Marshal(Message{Type: 5, Body: json.RawMessage(rawBody), Id: newClient.Id})
 			if err != nil {
 				log.Println(err)
 				return
 			}
 
-			for i, client := range pool.Clients[newClient.Id] {
+			pool.Broadcast <- byteData
 
-				if client.UserId == newClient.UserId {
-					toDelete = i
-				}
+			// Delete client from nested map using delete() function
+			pool.mu.Lock()
+			delete(pool.Clients[newClient.Id], newClient.UserId)
 
-				if client.Role != newClient.Role || client.UserId != newClient.UserId {
-					if err := wsutil.WriteServerText(client.Conn, byteData); err != nil {
-						log.Println(err)
-						return
-					}
-
-					// client.Conn.WriteJSON(Message{Type: 5, Body: map[string]interface{}{"user": newClient.UserId, "add": false}, Id: newClient.Id})
-				}
-
+			// Clean up empty nested maps
+			poolSize := 0
+			if len(pool.Clients[newClient.Id]) == 0 {
+				delete(pool.Clients, newClient.Id)
+			} else {
+				poolSize = len(pool.Clients[newClient.Id])
 			}
-			pool.Clients[newClient.Id] = append(pool.Clients[newClient.Id][:toDelete], pool.Clients[newClient.Id][toDelete+1:]...)
+			pool.mu.Unlock()
 
-			fmt.Println("Size of Connection Pool: ", len(pool.Clients[newClient.Id]))
+			// Close the connection
+			newClient.Conn.Close()
+
+			fmt.Println("Size of Connection Pool: ", poolSize)
 
 		case message := <-pool.Broadcast:
 			// Publish the message on "general" channel
 			pool.publishMessage(message, ds)
 
 		case <-ticker.C:
-			for _, v := range pool.Clients {
-
-				for _, client := range v {
-
-					if !client.IsAlive {
+			// Process clients incrementally to reduce memory pressure
+			pool.mu.RLock()
+			for _, clientsMap := range pool.Clients {
+				for _, client := range clientsMap {
+					// Check and update IsAlive status
+					if !client.IsAlive.Load() {
+						// Unlock before sending to channel to avoid blocking
+						pool.mu.RUnlock()
 						client.Pool.Unregister <- client
-						client.Conn.Close()
+						pool.mu.RLock()
 						continue
 					}
 
-					client.IsAlive = false
+					client.IsAlive.Store(false)
 
-					if err := wsutil.WriteServerBinary(client.Conn, []byte("1")); err != nil {
+					// Send heartbeat - do this while holding read lock is safe
+					// as we're not modifying the map structure
+					// Use:
+					if err := ws.WriteFrame(client.Conn, ws.NewPingFrame(nil)); err != nil {
 						log.Println(err)
-						return
+						continue
 					}
 				}
 			}
+			pool.mu.RUnlock()
 		}
 	}
 }
 
 // Redis Publish message functionality
 func (pool *Pool) publishMessage(payload []byte, ds *ds.DataSources) {
-	// payload, err := json.Marshal(msg)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
+
 	err := ds.Redis.Publish(ctx, PubSubGeneralChannel, payload).Err()
 
 	if err != nil {
@@ -185,29 +187,51 @@ func (pool *Pool) listenPubSubChannel(ds *ds.DataSources) {
 	for data := range ch {
 
 		msg := Message{}
+		payloadData := []byte(data.Payload)
 
-		if err := json.Unmarshal([]byte(data.Payload), &msg); err != nil {
+		if err := json.Unmarshal(payloadData, &msg); err != nil {
 			log.Println(err)
 			return
 		}
 
-		for _, client := range pool.Clients[msg.Id] {
-			if msg.Type == 1 && client.Details.Role == "Student" {
-				fmt.Println("Sending message to all students in Pool")
-				if err := wsutil.WriteServerText(client.Conn, []byte(data.Payload)); err != nil {
-					log.Println(err)
-					return
-				}
+		pool.mu.RLock()
 
-			} else if msg.Type == 4 && client.UserId == msg.To {
-				fmt.Println("Sending message to specific client in Pool")
-				if err := wsutil.WriteServerText(client.Conn, []byte(data.Payload)); err != nil {
-					log.Println(err)
-					return
+		switch msg.Type {
+		case 1, 3:
+			for _, client := range pool.Clients[msg.Id] {
+				fmt.Println("Sending message to all students in Pool")
+				if client.Details.Role == "Student" {
+					if err := wsutil.WriteServerText(client.Conn, payloadData); err != nil {
+						log.Println(err)
+						continue
+					}
 				}
 			}
 
+		case 4:
+			if client, ok := pool.Clients[msg.Id][msg.To]; ok {
+				fmt.Println("Sending message to specific client in Pool")
+				if err := wsutil.WriteServerText(client.Conn, payloadData); err != nil {
+					log.Println(err)
+					continue
+				}
+			}
+
+		case 5:
+			var body JoinLeaveBody
+			json.Unmarshal(msg.Body, &body)
+			joiningUserId := body.User
+			for userId, client := range pool.Clients[msg.Id] {
+				if userId != joiningUserId {
+					if err := wsutil.WriteServerText(client.Conn, payloadData); err != nil {
+						log.Println(err)
+						continue
+					}
+				}
+			}
 		}
+
+		pool.mu.RUnlock()
 
 	}
 }
