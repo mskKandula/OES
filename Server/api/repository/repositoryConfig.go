ba1ba14553d6f4190134862b93df779e0355eca0
Mohana sai krishna Kandula <73701479+mskKandula@users.noreply.github.com>
2026-05-06 11:49:44 +0530

    Refactor RepositoryConfig to remove RabbitMQ fields
    Removed RabbitMQ related fields from RepositoryConfig.

--- Server/api/repository/repositoryConfig.go
+++ Server/api/repository/repositoryConfig.go
@@ -6,14 +6,11 @@ import (
 
 	redis "github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type RepositoryConfig struct {
-	MySQLDB  *sql.DB
-	Redis    *redis.Client
-	RabbitMQ *amqp.Channel
-	Queue    amqp.Queue
+	MySQLDB *sql.DB
+	Redis   *redis.Client
 }
 
 // type Transaction interface {
